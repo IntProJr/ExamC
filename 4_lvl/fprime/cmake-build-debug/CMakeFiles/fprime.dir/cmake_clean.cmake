d7931ddd6d1afc29bea58c66a6b3151a1fc39e2a
Intpro <intprojr@yandex.ru>
2020-01-22 17:45:04 +0300

    fprime

---
+++ 4_lvl/fprime/cmake-build-debug/CMakeFiles/fprime.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fprime.dir/main.c.o"
+  "fprime"
+  "fprime.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fprime.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4_lvl/fprime/cmake-build-debug/CMakeFiles/fprime.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fprime.
+# This may be replaced when dependencies are built.
