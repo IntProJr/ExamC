9d1f8034cbf50e7a171a82fc8a02e2548e14d4ba
Intpro <intprojr@yandex.ru>
2020-01-22 18:25:59 +0300

    fprime well done

--- 4_lvl/fprime/main.c
+++ 4_lvl/fprime/main.c
@@ -1,7 +1,46 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrosalee <lrosalee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/22 18:25:40 by lrosalee          #+#    #+#             */
+/*   Updated: 2020/01/22 18:25:40 by lrosalee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_fprime(char *str)
 {
-	printf("Hello, World!\n");
+	int nb;
+	int div;
+
+	div = 2;
+	nb = atoi(str);
+	if (nb == 1)
+		return ;
+	while (div <= nb)
+	{
+		if (nb % div == 0)
+		{
+			printf("%d", div);
+			if (nb == div)
+				return ;
+			printf("*");
+			nb = nb / div;
+			div = 1;
+		}
+		div++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+		ft_fprime(argv[1]);
+	printf("\nHello, ft_fprime\n");
 	return 0;
 }
